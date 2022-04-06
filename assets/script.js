78560b3b8649e502d293c3f4e9735dacf25e0945
callanhunter <callan57hunter@gmail.com>
2022-04-06 16:45:34 -0700

    add files and event listener

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+localStorage.getItem
+var nineOclock = document.querySelector(".time-slot")
+
+element.addEventListener("click", nineTimeSlot);
+
+function nineTimeSlot() {
+    localStorage.setItem()
+}
