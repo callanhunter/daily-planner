4ccc31c3048cf37091beca6ec369189c84f75106
callanhunter <callan57hunter@gmail.com>
2022-04-07 18:05:31 -0700

    local storage update and README

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 # Description
 
-###
+### Ever have a hard time planning your day? We've all been there. With this daily planner you are able to plan your day with ease. From 9AM to 5PM, this planner will show you the current hour in grey, previous hours in red, and future hours in green.
 
 # Features
 
-###
+### This project includes
 
 # Usage
 
--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,15 @@ $("#time-block").text(gradDate);
 var btn9 = document.querySelector(".saveBtn9");
 var nineOclock = document.querySelector("input9");
 
-document.getElementById("input9").textContent = localStorage.getItem("nineslot")
+document.getElementById("input9").textContent = localStorage.getItem("nineslot");
+// document.getElementById("input10").textContent = localStorage.getItem("tenslot");
+// document.getElementById("input11").textContent = localStorage.getItem("elevenslot");
+// document.getElementById("input12").textContent = localStorage.getItem("twelveslot");
+// document.getElementById("input1").textContent = localStorage.getItem("oneslot");
+// document.getElementById("input2").textContent = localStorage.getItem("twoslot");
+// document.getElementById("input3").textContent = localStorage.getItem("threeslot");
+// document.getElementById("input4").textContent = localStorage.getItem("fourslot");
+// document.getElementById("input5").textContent = localStorage.getItem("fiveslot");
 
 btn9.addEventListener("click", nineTimeSlot);
 
@@ -15,41 +23,41 @@ btn9.addEventListener("click", nineTimeSlot);
 function nineTimeSlot(event) {
     var btn = event.target;
     // document.getElementById("item2").previousSibling.textContent;
-    var nineOclock = document.getElementById("btn9").previousSibling; 
+    var nineOclock = btn9.parentElement.previousSibling;
     // document.getElementById("input").innerHTML = nineOclock;
-    localStorage.setItem("nineslot", nineOclock.textContent)
+    localStorage.setItem("nineslot", nineOclock)
 };
 
 
-var btn10 = document.querySelector(".saveBtn10");
-var tenOclock = document.querySelector("input10");
+// var btn10 = document.querySelector(".saveBtn10");
+// var tenOclock = document.querySelector("input10");
 
-document.getElementById("input10").textContent = localStorage.getItem("tenslot")
+// document.getElementById("input10").textContent = localStorage.getItem("tenslot")
 
-btn10.addEventListener("click", tenTimeSlot);
-
-
-function tenTimeSlot(event1) {
-    var btn10 = event1.target;
-    // document.getElementById("item2").previousSibling.textContent;
-    var tenOclock = document.getElementById("btn10").previousSibling; 
-    // document.getElementById("input").innerHTML = nineOclock;
-    localStorage.setItem("tenslot", tenOclock.textContent)
-};
+// btn10.addEventListener("click", tenTimeSlot);
 
 
-var btn11 = document.querySelector(".saveBtn11");
-var elevenOclock = document.querySelector("input11");
+// function tenTimeSlot(event1) {
+//     var btn10 = event1.target;
+//     // document.getElementById("item2").previousSibling.textContent;
+//     var tenOclock = document.getElementById("btn10").previousSibling; 
+//     // document.getElementById("input").innerHTML = nineOclock;
+//     localStorage.setItem("tenslot", tenOclock.textContent)
+// };
 
-document.getElementById("input11").textContent = localStorage.getItem("elevenSlot")
 
-btn11.addEventListener("click", elevenTimeSlot);
+// var btn11 = document.querySelector(".saveBtn11");
+// var elevenOclock = document.querySelector("input11");
+
+// document.getElementById("input11").textContent = localStorage.getItem("elevenSlot")
+
+// btn11.addEventListener("click", elevenTimeSlot);
 
 
-function elevenTimeSlot(event) {
-    var btn11 = event.target;
-    // document.getElementById("item2").previousSibling.textContent;
-    var elevenOclock = document.getElementById("btn11").previousSibling; 
-    // document.getElementById("input").innerHTML = nineOclock;
-    localStorage.setItem("elevenSlot", elevenOclock.textContent)
-};
+// function elevenTimeSlot(event) {
+//     var btn11 = event.target;
+//     // document.getElementById("item2").previousSibling.textContent;
+//     var elevenOclock = document.getElementById("btn11").previousSibling; 
+//     // document.getElementById("input").innerHTML = nineOclock;
+//     localStorage.setItem("elevenSlot", elevenOclock.textContent)
+// };
